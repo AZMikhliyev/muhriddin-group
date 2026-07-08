1f258cad09435790f82e600bde22b86d752d9386
AZMikhliyev <musicytchannel261@gmail.com>
2026-07-09 01:41:42 +0500

    fix: app duplicate declaration error

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`✅ Server ${port}-portda ishlamoqda`);
 });
-const app = express();
 
 app.use(cors());
 app.use(express.json());
