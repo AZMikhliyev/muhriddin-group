d401007d8dac7637a20c52256b37ab1f47eff464
AZMikhliyev <musicytchannel261@gmail.com>
2026-07-09 03:55:06 +0500

    fix: force add ssl reject unauthorized

--- server.js
+++ server.js
@@ -18,13 +18,17 @@ app.use(express.static(path.join(__dirname, "public")));
 // =====================
 // MYSQL ULASH
 // =====================
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: { rejectUnauthorized: true }
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  // DIQQAT! Mana shu qismini qo'shing:
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 db.connect((err) => {
