0c69717b283ed5a425cf943474731eb24c48d797
AZMikhliyev <musicytchannel261@gmail.com>
2026-07-09 03:57:14 +0500

    fix: database host name error

--- server.js
+++ server.js
@@ -18,16 +18,15 @@ app.use(express.static(path.join(__dirname, "public")));
 // =====================
 // MYSQL ULASH
 // =====================
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: 'mysql',
-  // DIQQAT! Mana shu qismini qo'shing:
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
